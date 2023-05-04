6284db7ddfde8d33519da2e9ba941993fcf11e81
vorona80 <vorona_80@mail.ru>
2023-05-05 02:02:58 +0300

    Search2

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,10 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
